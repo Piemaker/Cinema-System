134d88507ed23f328bb043e61d279a0e66d17667
Mikel samir <michaelsamir1999@gmail.com>
2020-06-18 14:00:46 +0200

    add test using mockito

--- test/Statistics/MovieTest.java
+++ test/Statistics/MovieTest.java
@@ -17,8 +17,9 @@ import static org.junit.jupiter.api.Assertions.*;
 import jdk.nashorn.internal.ir.annotations.Ignore;
 import org.junit.Test;
 import static org.junit.Assert.*;
-//import static org.mockito.Mockito.*;
-
+import static org.mockito.Mockito.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 /**
  *
  * @author Michael Samir
@@ -29,10 +30,12 @@ public class MovieTest {
     String genre = "Comedy";
     double rating = 4.5 ;
   
+   
+    
+    
     /**
      * Test of equals method, of class Movie.
      */
-
     @Test
     public void testEquals() {
         System.out.println("equals");
@@ -43,8 +46,25 @@ public class MovieTest {
          assertEquals(expResult, result);
          //assertEquals( "equals dosen't work !" , !expResult, result);
         // TODO review the generated test code and remove the default call to fail.
-       
-       
+    }
+    /**
+     * Test of ResultSet constructor method, of class Movie.
+     */
+    
+     @Test 
+    public void testResultSet() throws SQLException{
+        /*fake database*/
+        ResultSet a= mock(ResultSet.class);
+        when(a.next()).thenReturn(true).thenReturn(false);
+        when(a.getInt("id")).thenReturn(id);
+        when(a.getString("name")).thenReturn(name);
+        when(a.getString("genre")).thenReturn(genre);
+        when(a.getDouble("rating")).thenReturn(rating);
+        /*new instance*/
+        Movie m = new Movie(a);
+        Movie m0 = new Movie(id, name, genre, rating);
+        /*test*/
+        assertTrue(m0.equals(m));
     }
 }
     
